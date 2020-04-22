908a6ca18a5446bd4f18b68d122598e5693cde2a
jtefix <56880173+jtefix@users.noreply.github.com>
2020-04-22 16:23:42 +0100

    MAIN

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Tokens
 import Grammar
+import TypeCheck
 import System.Environment
 import Control.Exception
 import System.IO
@@ -13,7 +14,10 @@ main' = do
     (file : _) <- getArgs
     contents <- readFile file
     putStrLn ("Text to be parsed: " ++ contents)
-    putStrLn ("Parsed text: " ++ show (parseCalc (alexScanTokens contents)))
+    let string = parseCalc (alexScanTokens contents)
+    putStrLn ("Parsed text: " ++ show string)
+    let typeOf = checker [] string
+    putStrLn ("typeCheck; " ++ show typeOf) 
 
 noParse :: ErrorCall -> IO ()
 noParse e = do
