4a97d14d38e16369b8551cbf80a4905eaa5ccc1a
jtefix <56880173+jtefix@users.noreply.github.com>
2020-04-27 17:54:58 +0100

    read from input

--- Main.hs
+++ Main.hs
@@ -7,25 +7,50 @@ import Eval
 import System.Environment
 import Control.Exception
 import System.IO
+import Data.List
+import qualified Data.Text    as Text
+import qualified Data.Text.IO as Text
 
-main :: IO ()
-main = catch main' noParse
+-- main :: IO ()
+-- main = catch main' noParse
 
-main' = do 
+-- main' = do 
+--     (file : _) <- getArgs
+--     contents <- readFile file
+--     putStrLn ("Text to be parsed: " ++ contents)
+--     let string = parseCalc (alexScanTokens contents)
+--     putStrLn ("Parsed text: " ++ show string)
+--     let typeOf = checker [] string
+--     putStrLn ("typeCheck: " ++ show typeOf) 
+--     putStrLn ("Type Checking Passed with type " ++ (printType typeOf) ++ "\n") 
+--     let result = evalLoop (string)
+--     putStrLn ("Evaluates to " ++ (unparse result) ++ "\n")
+
+
+-- noParse :: ErrorCall -> IO ()
+-- noParse e = do
+--     let err = show e
+--     hPutStr stderr err
+--     return ()
+
+multiZip :: [[a]] -> [[a]]
+multiZip [] = []
+multiZip ([]:xss) = multiZip xss
+multiZip ((x:xs):xss) = (x:[z|(z:_) <- xss]) : multiZip (xs : [t | (_:t) <- xss])
+
+splitOn :: Char -> String -> [String]
+splitOn c [] = []
+splitOn c ls = (takeWhile (/=c) ls) : splitOn' c (dropWhile (/=c) ls)
+ where splitOn' c [] = []
+       splitOn' c (x:[]) | x==c = [[]]
+       splitOn' c (x:xs) | x==c = splitOn c xs
+                         | otherwise = []
+
+main = do 
     (file : _) <- getArgs
-    contents <- readFile file
-    putStrLn ("Text to be parsed: " ++ contents)
-    let string = parseCalc (alexScanTokens contents)
-    putStrLn ("Parsed text: " ++ show string)
-    let typeOf = checker [] string
-    putStrLn ("typeCheck: " ++ show typeOf) 
-    putStrLn ("Type Checking Passed with type " ++ (printType typeOf) ++ "\n") 
-    let result = evalLoop (string)
-    putStrLn ("Evaluates to " ++ (unparse result) ++ "\n")
-
-
-noParse :: ErrorCall -> IO ()
-noParse e = do
-    let err = show e
-    hPutStr stderr err
-    return ()
+    contents <- fmap lines (readFile file)
+    let s1 = map (splitOn ' ') contents
+    let s2 = map (map read) s1 :: [[Int]]
+    let s3 = multiZip s2
+    putStrLn (show s3)
+  --  print $ splitOn "\n" contents
